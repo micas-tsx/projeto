b8b3548afec05b512110cd24dcea198606cb548e
micael-web <micaelabud21@gmail.com>
2023-07-20 15:59:31 -0300

    UPDATE-2

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { FaPlus } from 'react-icons/fa';
+
 // eslint-disable-next-line import/no-unresolved
-import './Main.css';
+import './main.css';
 
 export default class Main extends Component {
   state = {
@@ -21,9 +24,15 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de Tarefas</h1>
 
-        <form action="#">
-          <input onChange={this.handleChange} type="text" />
-          <button type="submit">Enviar</button>
+        <form action="#" className="form">
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={novaTarefa}
+          />
+          <button type="submit">
+            <FaPlus />
+          </button>
         </form>
       </div>
     );
